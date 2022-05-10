6cfc04681684cb4179df2db8c9b10215fa678933
yanghaibo <yanghaibo_97@163.com>
2022-05-10 20:08:45 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author 12622
+ * @create 2022-5-10 20:03
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
